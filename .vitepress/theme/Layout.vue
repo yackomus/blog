71118e7e5a45dd63e5b139836007e85e64a414ec
Yackomus <yackomus@gmail.com>
2025-03-04 20:43:12 -0300

    Added why not

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -12,17 +12,17 @@ const { page, frontmatter } = useData()
     <div class="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
       <nav class="flex justify-between items-center py-10 font-bold">
         <a class="text-xl" href="/" aria-label="The Vue Point">
-          <img
+          <!-- <img
             class="inline-block mr-2"
             style="width: 22.06px; height: 31px"
             alt="logo"
             src="/yackomus-subject.png"
-          />
-          <span
+          /> -->
+          <!-- <span
             v-if="!frontmatter.index"
             class="hidden md:inline dark:text-white"
             >Brasília</span
-          >
+          > -->
         </a>
         <div class="text-sm text-gray-500 dark:text-white leading-5">
           <!-- <a
--- index.md
+++ index.md
@@ -1,5 +1,5 @@
 ---
-title: Brasília
+title: Blog
 subtext: Comentários amostrais que demonstram a rotina da qual os burocratas brasileiros participam para tomar um cafezinho, cantar palavras vazias e trair a nação.
 index: true
 ---
